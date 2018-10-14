e34f98509cc44a65934d1eca51ae1272bd22b2b5
Misbahaslamsabrani <misbahaslamsabrani@gmail.com>
2018-10-15 02:53:28 +0500

    first commit

---
+++ src/components/dashboard.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class FrontCom extends Component {
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default FrontCom;
---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyDfa3C8UVRIXYFxfjTjpm3o1Itj8AGD3C8",
+    authDomain: "to-do-react-firebasedb.firebaseapp.com",
+    databaseURL: "https://to-do-react-firebasedb.firebaseio.com",
+    projectId: "to-do-react-firebasedb",
+    storageBucket: "to-do-react-firebasedb.appspot.com",
+    messagingSenderId: "329421369986"
+  };
